eb30115096a57f3ccea9fcebf7d9faf7103cd3dd
csprashant <csprashantphuse@gmail.com>
2021-06-18 23:03:35 +0530

    Initial commit project structure

---
+++ CarBookingSystem/src/test/java/com/nbs/CarBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
